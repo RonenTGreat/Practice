f3c3672c75ca3da5985ed6934823197da5bc27bf
ronentgreat <ronenh53@gmail.com>
2021-09-09 21:29:34 +0000

    Did some spelling corrections.

---
+++ Games/Dice_Simulator.py
@@ -0,0 +1,18 @@
+# This program simulates the rolling of a 6 sides dice
+
+import random
+
+rolling = True
+
+while rolling:
+    # Create a random number from 1 to 6.
+    dice = random.randint(1, 6)
+    print("Press Enter to roll or q to Quit.")
+    start = input(">>")
+
+    # Keeps rolling until user enters a q to quit
+    if start.lower() != "q":
+        print("You rolled a", dice)
+    else:
+        rolling = False
+print("Thank for Playing!!")
