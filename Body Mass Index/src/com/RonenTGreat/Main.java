c85906a151e41d9f03fac1824890ba1c5e3b5d08
ronentgreat <ronenh53@gmail.com>
2021-09-25 22:37:42 +0000

    completed

--- Body Mass Index/src/com/RonenTGreat/Main.java
+++ Body Mass Index/src/com/RonenTGreat/Main.java
@@ -11,5 +11,26 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         weight = scanner.nextFloat();
 
+        System.out.print("Enter your height (in cm): ");
+        height = scanner.nextFloat();
+
+        float bodyMassIndex = weight / height * height;
+
+        if(bodyMassIndex > 0 && bodyMassIndex < 18.5)
+            System.out.println("Your BMI is " + bodyMassIndex + "\n You are underweight. You should eat more foods.");
+
+        else if(bodyMassIndex >= 18.5 && bodyMassIndex < 25)
+            System.out.println("Your BMI is " + bodyMassIndex + "\n You are normal. Keep it up!");
+
+        else if(bodyMassIndex >= 25 && bodyMassIndex < 30)
+            System.out.println("Your BMI is " + bodyMassIndex + "\n You are overweight. " +
+                    "Decrease the foods you eat and exercise;");
+
+        else if(bodyMassIndex > 30)
+            System.out.println("Your BMI is " + bodyMassIndex + "\n You are obese. Go on a diet and exercise intensively");
+
+        else
+            System.out.println("Invalid inputs");
+
     }
 }
