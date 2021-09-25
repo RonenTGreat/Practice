c09bcbc5bb4c358a901092d187db962cc76d1278
ronentgreat <ronenh53@gmail.com>
2021-09-25 22:18:34 +0000

    GE

---
+++ Body Mass Index/src/com/RonenTGreat/Main.java
@@ -0,0 +1,15 @@
+package com.RonenTGreat;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    float weight;
+	    float height;
+        System.out.print("Enter your weight(in kg): ");
+        Scanner scanner = new Scanner(System.in);
+        weight = scanner.nextFloat();
+
+    }
+}
