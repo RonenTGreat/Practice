827c24919d086336c92e73ba5d576ecce434eaae
ronentgreat <ronenh53@gmail.com>
2021-10-11 19:57:35 +0000

    will continue tomorrow

--- Games/src/com/company/Main.java
+++ Games/src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
 
+
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while(true){
+            String[] choice = {"rock", "paper", "scissor"};
+            int computerScore = 0;
+            int playerScore = 0;
+            java.util.Random random = new java.util.Random();
+            int randomTake = random.nextInt(choice.length);
+            String computer = (choice[randomTake]);
+            System.out.println("Rock, Paper or Scissor?");
+            System.out.print(">> ");
+            String player = input.next();
+            if(player.toLowerCase().strip().equals(computer))
+                System.out.println("It's a tie!");
+            else if(player.equalsIgnoreCase("rock")){
+                if(computer.equals("paper")){
+                    System.out.println("You lose! " + computer + " covers " + player + ".");
+                    computerScore += 1;
+                }
+                else{
+                    System.out.println("You win! " + player + " destroys " + computer + ".");
+                    playerScore += 1 ;
+                }
+            }
+        }
 
     }
 }
